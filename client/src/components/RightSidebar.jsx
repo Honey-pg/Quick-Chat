705f4343e976241201db4aba0d4ad1bfe1922551
Prerak Yadav <prerakyadav1507@gmail.com>
2025-09-14 19:39:38 +0530

    added vercel.json

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { use, useContext, useEffect, useState } from 'react'
 import avatar_icon from '../assets/avatar_icon.png'
 import imagesdata from '../assets/imagesDummydata'
-const RightSidebar = ({selecteduser,setselecteduser}) => {
-  console.log(imagesdata)
+import { chatContext } from '../../context/chatContext'
+import { authContext } from '../../context/authContext'
+const RightSidebar = () => {
+   const {selecteduser,messages}=useContext(chatContext)
+   const {logout,onlineUsers}=useContext(authContext)
+   const [msgImages,setmsgImages]=useState([])
+
+   //get all the images of the user
+   useEffect(()=>{
+    setmsgImages(messages.filter(msg=> msg.image).map(msg=>msg.image))
+   },[messages])
   return selecteduser && (
      <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selecteduser ? "max-md:hidden":""}`}>
       <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
         <img src={selecteduser?.avatar || avatar_icon} alt="" 
         className=' w-20 aspect-[1/1] rounded-full'/>
         <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
-          <p className='w-2 h-2 rounded-full bg-green-500'></p>
+          {onlineUsers.includes(selecteduser._id) && <p className='w-2 h-2 rounded-full bg-green-500'></p>}
           {selecteduser.fullName}</h1>
           <p className='px-10 mx-auto'>{selecteduser.bio}</p>
       </div>
@@ -17,14 +26,14 @@ const RightSidebar = ({selecteduser,setselecteduser}) => {
       <div className='px-5 text-xs'>
           <p>Media</p>
       <div className='mt-2 grid grid-cols-2 gap-4 opacity-80 max-h-[200px] overflow-y-scroll'>
-        {imagesdata.map((url,index)=>(
+        {msgImages.map((url,index)=>(
           <div keys ={index} onClick={()=> window.open(url)} className='cursor-pointer rounded'>
           <img src={url} alt="" className='h-full rounded-md'/>
           </div>
         ))}
       </div>
     </div>  
-    <button className='absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-900 to-violet-500 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer'>Logout</button>
+    <button onClick={()=>logout()} className='absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-900 to-violet-500 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer'>Logout</button>
       </div>
     
     )
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import ChatComponents from '../components/ChatComponents'
 import Sidebar from '../components/Sidebar'
 import RightSidebar from '../components/RightSidebar'
+import { chatContext } from '../../context/chatContext'
 
 
 const HomePage = () => {
-  const [selecteduser,setselecteduser]=useState(false);
+  const {selecteduser}=useContext(chatContext);
 
   return (
     <div className='border h-screen w-full sm:px-[15%] sm:py-[5%]'>
@@ -13,7 +14,7 @@ const HomePage = () => {
         ${selecteduser ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]" : "md:grid-cols-2"}`}>
         <Sidebar />
         <ChatComponents />
-        <RightSidebar  selecteduser={selecteduser} setselecteduser={setselecteduser}/>
+        <RightSidebar  />
       </div>
     </div>
   )
